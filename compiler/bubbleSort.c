e1b4c0ad264f8a49c1a198ae2e03ecd034cde97f
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2021-04-08 20:25:12 +0200

    fix test

--- compiler/bubbleSort.c
+++ compiler/bubbleSort.c
@@ -1,18 +1,17 @@
 void printInt(int n);
 int main (){
     int array[10];
-    array[0] = 9;
-    array[1] = 8;
-    array[2] = 7;
-    array[3] = 6;
-    array[4] = 5;
+    array[0] = 5;
+    array[1] = 3;
+    array[2] = 123;
+    array[3] = 43;
+    array[4] = 78;
     array[5] = 4;
-    array[6] = 3;
-    array[7] = 2;
+    array[6] = 98;
+    array[7] = 63;
     array[8] = 1;
-    array[9] = 0;
+    array[9] = 27;
     int temp;
-    int k;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10 - 1; j++) {
             temp = array[j + 1];
@@ -22,7 +21,7 @@ int main (){
             }
         }
         for (int k = 0; k < 10; k++) {
-            putchar(array[k] + 48);
+            printInt(array[k]);
         }
         putchar(10);
     }
@@ -30,14 +29,16 @@ int main (){
 }
 void printInt(int n)
 {
-    int a, b, c;
-    a = n - n/10*10;
-    n = n/10;
-    b = n - n/10*10;
-    n = n/10;
-    c = n;
-    putchar(c + 48);
-    putchar(b + 48);
-    putchar(a + 48);
+    int nb [10];
+    int length = 0;
+    while( n/10 | n-n/10*10 )
+    {
+        nb[length] = n - n/10*10;
+        n = n/10;
+        ++length;
+    }
+    for(int i = length - 1; i >= 0; --i){
+        putchar(nb[i] + 48);
+    }
     putchar(32);
 }
